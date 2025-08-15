9f3143ccaf79dd7dd2f3fd79457b2a5c5d160fcc
Sharon Lee <ssmlee10@gmail.com>
2025-08-15 00:26:00 -0700

    connect db with front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
+import * as trackServices from './services/trackService.js';
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+
+  const fetchTracks = async () => {
+    const fetchedTracks = await trackServices.index();
+    setTracks(fetchedTracks);
+
+  };
+
+  fetchTracks();
+
+}, []);
+
+return <h1>Hello World!</h1>
+
 };
+
+
 
 export default App;
--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,1 +1,19 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+
+};
+
+// logs all of the tracks
+console.log(await index());
+
+
+export {
+    index,
+};
